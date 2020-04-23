f04ca14dcc011a669fb5835323817ffc1a84c35e
Matthew Taylor <majikpig@gmail.com>
2020-04-22 21:20:10 -0400

    more tests

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -31,7 +31,7 @@ const TodoDiv = styled.div`
 `;
 
 function Todo(props) {
-  const [dueDate, setDueDate] = useState(props.task.dueDate||null);
+  const [dueDate, setDueDate] = useState(props.task.dueDate||undefined);
   const [isOverdue, setIsOverdue] = useState();
 
   const changeDueDate = (event) => {
@@ -51,7 +51,7 @@ function Todo(props) {
     <TodoDiv>
       {props.task.completed?<input name={props.task.id} type="checkbox" checked onChange={()=>props.markComplete(props.task.id,false)} />:<input name={props.task.id} type="checkbox" onChange={()=>props.markComplete(props.task.id,true)} />}
       {props.task.completed?<label htmlFor={props.task.id} className="strikeThru" onClick={()=>props.markComplete(props.task.id,false)} >{props.task.item}</label>:<label htmlFor={props.task.id} onClick={()=>props.markComplete(props.task.id,true)} >{props.task.item}</label>}
-      {props.task.completed&&props.task.compDate?<span>Completed: <Moment format={dateFormat}>{props.task.compDate}</Moment></span>:isItBeforeNow(props.task.dueDate)?<span className="overdue">Overdue! <input type="date" value={dueDate} onChange={saveDueDate} onSubmit={saveDueDate} /></span>:<span>Due date: <input type="date" value={dueDate} onChange={saveDueDate} onSubmit={saveDueDate} /></span>}
+      {props.task.completed&&props.task.compDate?<span data-testid="completedSpan">Completed: <Moment data-testid="moment" format={dateFormat}>{props.task.compDate}</Moment></span>:isItBeforeNow(props.task.dueDate)?<span data-testid="dueDateSpan" className="overdue">Overdue! <input data-testid="dueDateInput" type="date" value={dueDate} onChange={saveDueDate} onSubmit={saveDueDate} /></span>:<span data-testid="dueDateSpan">Due date: <input data-testid="dueDateInput" type="date" value={dueDate} onChange={saveDueDate} onSubmit={saveDueDate} /></span>}
     </TodoDiv>
   );
 }
--- todo/src/tests/Todo.test.js
+++ todo/src/tests/Todo.test.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import Todo from '../components/Todo';
+import moment from 'moment';
 
 const mockFunction = jest.fn();
+const mockSaveDueDate = jest.fn();
 
 const sampleTask = {
   item: 'Sample task',
   completed: false,
-  id: 3892987589
+  id: 3892987589,
+  dueDate: '2020-04-21'
 };
 
 test('renders without crashing', () => {
-  render(<Todo task={sampleTask} />);
+  render(<Todo task={sampleTask} saveDueDate={mockSaveDueDate}  />);
 });
 
 test('renders task text', () => {
-  const { getByText } = render(<Todo task={sampleTask} />);
+  const { getByText } = render(<Todo task={sampleTask} saveDueDate={mockSaveDueDate} />);
   const taskElement = getByText(/sample task/i);
   expect(taskElement).toBeInTheDocument();
 });
 
 test('renders uncompleted task correctly', () => {
-  const { container, getByText } = render(<Todo task={sampleTask} />);
+  const { container, getByText } = render(<Todo task={sampleTask} saveDueDate={mockSaveDueDate} />);
   const checkBoxElement = container.querySelector('input');
   expect(checkBoxElement.checked).toBeFalsy();
   const taskElement = getByText(/sample task/i);
@@ -29,16 +32,32 @@ test('renders uncompleted task correctly', () => {
   expect(taskElement.className).not.toBe('strikeThru');
 });
 
+test('renders overdue task correctly', ()=> {
+  const { getByTestId } = render(<Todo task={sampleTask} saveDueDate={mockSaveDueDate} />);
+  const dueDateSpan = getByTestId('dueDateSpan');
+  expect(dueDateSpan.textContent).toBe('Overdue! ');
+  expect(dueDateSpan.textContent).not.toBe('Due date: ')
+});
+
+test('renders not overdue task correctly', ()=> {
+  const { getByTestId } = render(<Todo task={{...sampleTask, dueDate:'9999-12-31'}} saveDueDate={mockSaveDueDate} />);
+  const dueDateSpan = getByTestId('dueDateSpan');
+  expect(dueDateSpan.textContent).not.toBe('Overdue! ');
+  expect(dueDateSpan.textContent).toBe('Due date: ')
+});
+
 test('renders completed task correctly', () => {
-  const { container, getByText } = render(<Todo task={{...sampleTask, completed: true}} />);
+  const { container, getByText, getByTestId } = render(<Todo task={{...sampleTask, completed: true, compDate:'2020-04-23T01:19:09.783Z'}} saveDueDate={mockSaveDueDate} />);
   const checkBoxElement = container.querySelector('input');
   expect(checkBoxElement.checked).toBeTruthy();
   const taskElement = getByText(/sample task/i);
   expect(taskElement.className).toBe('strikeThru');
+  const completedSpan = getByTestId('completedSpan');
+  expect(completedSpan.textContent).toBe('Completed: 04/22/2020 9:19 PM');
 });
 
 test('calls props.markComplete fn when clicked', () => {
-  const { container, getByText } = render(<Todo task={sampleTask} markComplete={mockFunction} />);
+  const { container, getByText } = render(<Todo task={sampleTask} markComplete={mockFunction} saveDueDate={mockSaveDueDate} />);
   const checkBoxElement = container.querySelector('input');
   const taskElement = getByText(/sample task/i);
   fireEvent.click(taskElement);
